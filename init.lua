7dc035edb6897b24e7d9427d2715d2adebb15203
tumeninodes <tumeninodes@gmial.com>
2026-08-02 23:04:25 -0400

    Update licenses and audio files

--- init.lua
+++
@@ -1,7 +0,0 @@
-dofile(minetest.get_modpath("build_pack_01").."/arches.lua")
-
-dofile(minetest.get_modpath("build_pack_01").."/super_high.lua")
-
-dofile(minetest.get_modpath("build_pack_01").."/single_arch_doors.lua")
-
-dofile(minetest.get_modpath("build_pack_01").."/thin_walls.lua")
